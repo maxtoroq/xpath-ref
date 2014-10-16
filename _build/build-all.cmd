043c4adc19f6ed8ebfca9067d6a0a9767af411dd
Max Toro <maxtoroq@gmail.com>
2014-10-16 13:14:10 -0300

    use cached file

--- _build/build-all.cmd
+++ _build/build-all.cmd
@@ -2,4 +2,4 @@
 cd %~dp0
 
 call .\download-specs.cmd
-..\packages\Saxon-HE.9.5.1.6\tools\Transform.exe -s:http://www.w3.org/TR/2014/REC-xpath-functions-30-20140408/xpath-functions-30.xml -xsl:..\_xsl\all-functions.xsl
+..\packages\Saxon-HE.9.5.1.6\tools\Transform.exe -s:.\specs\xpath-functions-30.xml -xsl:..\_xsl\all-functions.xsl
