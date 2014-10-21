baba22ad7b9f47daf9a768900ebba4d5348aa60d
Max Toro <maxtoroq@gmail.com>
2014-10-21 16:36:43 -0300

    removed unnecessary commands

--- _build/download-specs.cmd
+++ _build/download-specs.cmd
@@ -1,6 +1,3 @@
-@echo off
-cd %~dp0
-
 IF NOT EXIST specs md specs
 
 IF NOT EXIST .\specs\xpath-functions-30.xml @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; (New-Object System.Net.WebClient).DownloadFile('http://www.w3.org/TR/2014/REC-xpath-functions-30-20140408/xpath-functions-30.xml', '.\specs\xpath-functions-30.xml')"
